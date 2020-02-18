ad02a7c66cff2de600191cdf5d1c67de71e19122
Florin Mihalache <florinrmfm3@gmail.com>
2020-02-19 01:15:30 +0200

    Added method

--- Lab2/main.py
+++ Lab2/main.py
@@ -1,17 +1,27 @@
 class Song:
     # self este echivalentul lui this din Java
     # __init__ este constructorul unei clase din Python
-    def __init__(self, artist, title):
+    def __init__(self, artist, title, year):
         self.artist = artist
         self.title = title
+        self.year = year
+
 
     # echivalentul toString() din Java
     def __str__(self):
         return self.title + " by " + self.artist
 
+
+    def play(self):
+        print('Now playing %s by %s, released in %d' %(self.title, self.artist, self.year))
+
+
 def main():
-    song = Song('Eminem', 'Killshot')
+    song = Song('Eminem', 'Killshot', 2018)
     print(song)
+    # calling a method
+    song.play()
+
 
 if __name__ == '__main__':
     main()
