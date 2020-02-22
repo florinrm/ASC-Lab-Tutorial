02196c9d4fc5833c2abeab248e8b4e0ac8c6adfe
Florin Mihalache <florinrmfm3@gmail.com>
2020-02-22 12:35:52 +0200

    Added threading code

--- Lab2/main.py
+++ Lab2/main.py
@@ -1,3 +1,5 @@
+from threading import Thread
+
 class Song:
     # self este echivalentul lui this din Java
     # __init__ este constructorul unei clase din Python
@@ -15,12 +17,33 @@ class Song:
     def play(self):
         print('Now playing %s by %s, released in %d' %(self.title, self.artist, self.year))
 
+    @staticmethod
+    def class_method():
+        print('static method')
+
+class MyThread(Thread):
+    def __init__(self, tid):
+        Thread.__init__(self)
+        self.tid = tid
+
+    def run(self):
+        print("Thread with id %d" %(self.tid))
 
 def main():
     song = Song('Eminem', 'Killshot', 2018)
     print(song)
     # calling a method
     song.play()
+    Song.class_method()
+
+    thread1 = MyThread(1)
+    thread2 = MyThread(2)
+    
+    thread1.start()
+    thread2.start()
+
+    thread1.join()
+    thread2.join()
 
 
 if __name__ == '__main__':
